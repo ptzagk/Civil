d9d1e309cc3ce220025dbe242f1943d1b1ec3000
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-08-10 14:08:42 -0400

    fixing unit tests

--- packages/contracts/test/tcr/government/get.ts
+++ packages/contracts/test/tcr/government/get.ts
@@ -4,17 +4,20 @@ import { configureChai } from "@joincivil/dev-utils";
 import * as utils from "../../utils/contractutils";
 import { BigNumber } from "bignumber.js";
 const Government = artifacts.require("Government");
+const PLCRVoting = artifacts.require("PLCRVoting");
 configureChai(chai);
 const expect = chai.expect;
 
 contract("Government", accounts => {
   describe("Function: get", () => {
-    const [JAB] = accounts;
+    const [JAB, nobody, voter] = accounts;
     let registry: any;
     let government: any;
+    let voting: any;
     beforeEach(async () => {
       registry = await utils.createAllCivilTCRInstance(accounts, JAB);
       government = await Government.at(await registry.government());
+      voting = await PLCRVoting.at(await registry.voting());
     });
 
     it("judgeAppealLen value should be same as in config after deployment", async () => {
@@ -26,11 +29,14 @@ contract("Government", accounts => {
     });
 
     it("judgeAppealLength should be new value after processing proposal", async () => {
-      const propId = await government.proposeReparameterization("judgeAppealLen", 100, { from: JAB });
-      await utils.advanceEvmTime(
-        utils.paramConfig.govtPCommitStageLength + utils.paramConfig.govtPRevealStageLength + 1,
-      );
-      await government.processProposal(propId);
+      const receipt = await government.proposeReparameterization("judgeAppealLen", 100, { from: JAB });
+
+      const { propID, pollID } = receipt.logs[0].args;
+      await utils.commitVote(voting, pollID, "1", "10", "420", voter);
+      await utils.advanceEvmTime(utils.paramConfig.govtPCommitStageLength + 1);
+      await voting.revealVote(pollID, "1", "420", { from: voter });
+      await utils.advanceEvmTime(utils.paramConfig.govtPRevealStageLength + 1);
+      await government.processProposal(propID, { from: nobody });
       const judgeAppealLength = await government.get("judgeAppealLen");
       expect(judgeAppealLength).to.be.bignumber.equal(
         new BigNumber(100),
